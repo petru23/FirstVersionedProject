cf0e46ccc37ad1aba9251f5682cbcc4ac8f03578
luxkun <luciano.ferraro@gmail.com>
2017-02-17 10:51:50 +0100

    Bombaaaaah

---
+++ Assets/RuotaStranoEFaiCoseBomba.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuotaStranoEFaiCoseBomba : MonoBehaviour {
+    private float accumulator;
+
+    void Awake ()
+	{
+	}
+	
+	void Update ()
+	{
+	    accumulator += Time.deltaTime;
+	    transform.forward += Vector3.one * Mathf.Cos(accumulator) * 0.01f;
+        transform.localScale += Vector3.one * Mathf.Sin(accumulator) * 0.01f;
+    }
+}
